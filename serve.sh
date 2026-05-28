5c2b8ddec681370bc5a31e699cc3dd78b519055f
lars <nardis55five@gmail.com>
2026-05-28 19:35:43 +0000

    asd

--- serve.sh
+++ serve.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DIR="$(cd "$(dirname "$0")" && pwd)"
 PORT=${1:-8082}
 echo "Serving perps-backtrack at http://$(hostname -I | awk '{print $1}'):$PORT"
-python3 -m http.server "$PORT"
+cd "$DIR" && python3 -m http.server "$PORT"
